1451447d0f686fb3353d0881a7863f36b665ff7b
sabrinahaddads <81028153+sabrinahaddads@users.noreply.github.com>
2023-10-24 17:06:24 +0200

    score.c

--- score.c
+++ score.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TABLEAU_SCORES_TAILLE 3
+#define FICHIER_SCORES "jeuhighscore.txt"
+
 typedef struct {
     int classement;
     char nom[5];
     double chrono;
 } Score;
 
-// Fonction de tri pour les meilleurs scores (tri par chrono croissant)
 void trierMeilleursScores(Score scores[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -26,12 +28,12 @@ int main() {
     noecho();
     cbreak();
 
-    // Chargez les meilleurs scores depuis le fichier jeuhighscore.txt
-    Score meilleursScores[3];
-    FILE* fichier = fopen("jeuhighscore.txt", "r");
+    Score meilleursScores[TABLEAU_SCORES_TAILLE];
+
+    FILE* fichier = fopen(FICHIER_SCORES, "r");
     if (fichier) {
-        for (int i = 0; i < 3; i++) {
-            if (fscanf(fichier, "%d %s %lf\n", &meilleursScores[i].classement,
+        for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
+            if (fscanf(fichier, "%d %4s %lf\n", &meilleursScores[i].classement,
                 meilleursScores[i].nom, &meilleursScores[i].chrono) != 3) {
                 printw("Erreur de lecture du fichier de scores.\n");
                 refresh();
@@ -41,36 +43,44 @@ int main() {
             }
         }
         fclose(fichier);
-    }
-    else {
-        // Le fichier de scores n'existe pas, initialisez les scores
-        for (int i = 0; i < 3; i++) {
+    } else {
+        printw("Le fichier de scores n'existe pas. Création du fichier...\n");
+        refresh();
+        // Créez le fichier de scores avec des scores par défaut
+        fichier = fopen(FICHIER_SCORES, "w");
+        for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
             meilleursScores[i].classement = i + 1;
             strcpy(meilleursScores[i].nom, "AAA");
-            meilleursScores[i].chrono = 999.9;  // Une valeur élevée pour l'initialisation
+            meilleursScores[i].chrono = 999.9;  // Valeur élevée pour l'initialisation
+            fprintf(fichier, "%d %4s %.1lf\n", meilleursScores[i].classement,
+                meilleursScores[i].nom, meilleursScores[i].chrono);
         }
+        fclose(fichier);
     }
 
     // Simulation d'une partie victorieuse avec un chrono de 80.0
     double chronoPartie = 80.0;
-    if (chronoPartie < meilleursScores[2].chrono) {
+    if (chronoPartie < meilleursScores[TABLEAU_SCORES_TAILLE - 1].chrono) {
         char nomJoueur[5];
         printw("Bravo, vous avez battu un meilleur chrono ! Entrez votre nom (4 caractères) : ");
         refresh();
         getnstr(nomJoueur, 4);
 
         // Mettez à jour le tableau des meilleurs scores
-        strcpy(meilleursScores[2].nom, nomJoueur);
-        meilleursScores[2].chrono = chronoPartie;
+        meilleursScores[TABLEAU_SCORES_TAILLE - 1].classement = 0;  // Indicateur que c'est un score du joueur
+        strcpy(meilleursScores[TABLEAU_SCORES_TAILLE - 1].nom, nomJoueur);
+        meilleursScores[TABLEAU_SCORES_TAILLE - 1].chrono = chronoPartie;
 
         // Triez le tableau des meilleurs scores
-        trierMeilleursScores(meilleursScores, 3);
+        trierMeilleursScores(meilleursScores, TABLEAU_SCORES_TAILLE);
 
         // Écrivez les trois meilleurs scores dans le fichier jeuhighscore.txt
-        FILE* fichier = fopen("jeuhighscore.txt", "w");
+        fichier = fopen(FICHIER_SCORES, "w");
         if (fichier) {
-            for (int i = 0; i < 3; i++) {
-                fprintf(fichier, "%d %s %.1lf\n", i + 1, meilleursScores[i].nom, meilleursScores[i].chrono);
+            for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
+                if (meilleursScores[i].classement > 0) {
+                    fprintf(fichier, "%d %4s %.1lf\n", i + 1, meilleursScores[i].nom, meilleursScores[i].chrono);
+                }
             }
             fclose(fichier);
         }
@@ -78,8 +88,10 @@ int main() {
 
     // Affichage des meilleurs scores
     printw("Meilleurs Scores :\n");
-    for (int i = 0; i < 3; i++) {
-        printw("%d. %s %.1lf\n", meilleursScores[i].classement, meilleursScores[i].nom, meilleursScores[i].chrono);
+    for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
+        if (meilleursScores[i].classement > 0) {
+            printw("%d. %4s %.1lf\n", meilleursScores[i].classement, meilleursScores[i].nom, meilleursScores[i].chrono);
+        }
     }
     refresh();
 
