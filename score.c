a1801eea9465432bb92da989ba5d49c99c5d59ec
sabrinahaddads <81028153+sabrinahaddads@users.noreply.github.com>
2023-11-13 13:23:57 +0100

    Update score.c

--- score.c
+++ score.c
@@ -6,11 +6,11 @@
 
 #define TABLEAU_SCORES_TAILLE 3
 #define FICHIER_SCORES "jeuhighscore.txt"
-
+#define TEMPS_JOUEUR 75.0
 
 typedef struct {
     int classement;
-    char nom[5 + 1]; // Augmentation de la taille pour inclure le caractère nul
+    char nom[5]; // Augmentation de la taille pour inclure le caractère nul
     double chrono;
 } Score;
 
@@ -32,50 +32,76 @@ void ajusterClassements(Score scores[], int n) {
     }
 }
 
+void initialiserMeilleursScores(Score scores[], int n) {
+    for (int i = 0; i < n; i++) {
+        scores[i].classement = i + 1;
+        strcpy(scores[i].nom, "AAA");
+        scores[i].chrono = 999.9;
+    }
+}
+
 int main() {
     initscr();
     noecho();
     cbreak();
+
+    // Création de la fenêtre avec des bordures
+    WINDOW *scoreWindow = newwin(TABLEAU_SCORES_TAILLE + 2, 25, 1, 1);
+    box(scoreWindow, 0, 0);
+
+    // Affichage de la fenêtre
+    wrefresh(scoreWindow);
 
     Score meilleursScores[TABLEAU_SCORES_TAILLE];
 
     FILE *fichier = fopen(FICHIER_SCORES, "r");
 
     if (!fichier) {
-        printw("Le fichier de scores n'existe pas.\n");
+        printw("Le fichier de scores n'existe pas. Création du fichier...\n");
         refresh();
         getch();
-        endwin();
-        return 1;
-    }
 
-    for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
-        if (fscanf(fichier, "%d %4s %lf\n", &meilleursScores[i].classement, meilleursScores[i].nom, &meilleursScores[i].chrono) != 3) {
-            printw("Erreur de lecture du fichier de scores.\n");
-            refresh();
-            getch();
+        // Initialiser les meilleurs scores avec des valeurs par défaut
+        initialiserMeilleursScores(meilleursScores, TABLEAU_SCORES_TAILLE);
+
+        // Écrire les valeurs par défaut dans le fichier
+        fichier = fopen(FICHIER_SCORES, "w");
+        if (fichier) {
+            for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
+                fprintf(fichier, "%d %4s %.1lf\n", meilleursScores[i].classement, meilleursScores[i].nom, meilleursScores[i].chrono);
+            }
             fclose(fichier);
-            endwin();
-            return 1;
         }
+    } else {
+        // Charger les meilleurs scores depuis le fichier
+        for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
+            if (fscanf(fichier, "%d %4s %lf\n", &meilleursScores[i].classement, meilleursScores[i].nom, &meilleursScores[i].chrono) != 3) {
+                printw("Erreur de lecture du fichier de scores.\n");
+                refresh();
+                getch();
+                fclose(fichier);
+                endwin();
+                return 1;
+            }
+        }
+        fclose(fichier);
     }
-    fclose(fichier);
 
     double chronoPartie = TEMPS_JOUEUR;
 
     if (chronoPartie < meilleursScores[TABLEAU_SCORES_TAILLE - 1].chrono) {
-        char nomJoueur[5 + 1];
+        char nomJoueur[5];
         printw("Bravo, vous avez battu un meilleur chrono ! Entrez votre nom (4 caractères SEULEMENT!) : ");
         refresh();
-        getnstr(nomJoueur, 4);
+        getnstr(nomJoueur,4);
 
-        // Ajouter le nouveau score à la fin du tableau
-        meilleursScores[TABLEAU_SCORES_TAILLE].classement = 0;
-        strcpy(meilleursScores[TABLEAU_SCORES_TAILLE].nom, nomJoueur);
-        meilleursScores[TABLEAU_SCORES_TAILLE].chrono = chronoPartie;
+        // Remplacer le dernier score du tableau
+        meilleursScores[TABLEAU_SCORES_TAILLE - 1].classement = 0;
+        strcpy(meilleursScores[TABLEAU_SCORES_TAILLE - 1].nom, nomJoueur);
+        meilleursScores[TABLEAU_SCORES_TAILLE - 1].chrono = chronoPartie;
 
         // Trier le tableau
-        trierMeilleursScores(meilleursScores, TABLEAU_SCORES_TAILLE;
+        trierMeilleursScores(meilleursScores, TABLEAU_SCORES_TAILLE);
 
         // Ajuster les classements
         ajusterClassements(meilleursScores, TABLEAU_SCORES_TAILLE);
@@ -90,11 +116,13 @@ int main() {
         }
     }
 
-    printw("Meilleurs Scores :\n");
+    // Affichage des scores dans la fenêtre avec des bordures
+    mvwprintw(scoreWindow, 1, 1, "Meilleurs Scores :");
     for (int i = 0; i < TABLEAU_SCORES_TAILLE; i++) {
-        printw("%d. %4s %.1lf\n", meilleursScores[i].classement, meilleursScores[i].nom, meilleursScores[i].chrono);
+        mvwprintw(scoreWindow, i + 2, 1, "%d. %4s %.1lf", meilleursScores[i].classement, meilleursScores[i].nom, meilleursScores[i].chrono);
     }
-    refresh();
+
+    wrefresh(scoreWindow);
 
     getch();
     endwin();
