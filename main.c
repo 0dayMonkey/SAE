417060356dedda5cea3818b0e3529ede900e9f42
Capri'☼ <95378035+0dayMonkey@users.noreply.github.com>
2023-11-30 09:46:55 +0100

    Add files via upload
    Refonte complete du menu, changement de couleur, de logique, de fonctionnement et surtout : les selections fonctionnent maintenant

--- main.c
+++ main.c
@@ -1,189 +1,92 @@
 #include <ncurses.h>
 #include <string.h>
-#include <pthread.h>
-#include <unistd.h>
 
-typedef struct {
-    int minutes;
-    int seconds;
-    int milliseconds;
-} Timer;
-
-void* updateTimer(void* arg) {
-    Timer* timer = (Timer*)arg;
-
-    while (1) {
-        usleep(10000);
-        timer->milliseconds += 10;
-
-        if (timer->milliseconds >= 1000) {
-            timer->milliseconds -= 1000;
-            timer->seconds++;
-
-            if (timer->seconds >= 60) {
-                timer->seconds -= 60;
-                timer->minutes++;
-            }
-        }
-    }
-
-    return NULL;
-}
-pthread_mutex_t ncurses_mutex;
-void* updateText(void* arg) {
-    WINDOW* mainWin = (WINDOW*)arg;
-    char text[] = "Le but du jeu est simple : Trouve un couple de carte le plus rapidement possible ! Fais attention, le temps passe vite chef ! ";
-    int textLength = strlen(text);
-    int winWidth = getmaxx(mainWin);
-
-    while (1) {
-        for (int i = 0; i < textLength; i++) {
-            int x, y;
-            getyx(mainWin, y, x);
-            pthread_mutex_lock(&ncurses_mutex);
-    werase(mainWin);
-
-            for (int j = 0; j < winWidth; j++) {
-                int idx = (i + j) % textLength;
-                mvwaddch(mainWin, 2, j + 1, text[(idx + textLength) % textLength]);
-            }
-
-            box(mainWin, 0, 0);
-            wrefresh(mainWin);
-    pthread_mutex_unlock(&ncurses_mutex);
-            usleep(300000);  // ça dort ouuuu??
-        }
-    }
-}
-
-
-
+#include "jeu.c"
 int main() {
-    pthread_mutex_init(&ncurses_mutex, NULL);
     initscr();
     noecho();
     curs_set(0);
-    start_color();  // Activer la couleur
-    init_pair(1, COLOR_YELLOW, COLOR_BLACK);  // paire de couleur (jaune sur fond noir pour l'instant, on change peut etre après)
+    cbreak();
+    keypad(stdscr, TRUE);
 
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
+    start_color();
 
-    int haut = 30;
-    int large = 50;
-    int mainWinWidth = (xMax / 2)-3;
-    int mainWinHeight = (yMax/6)-2;
-    int mainWinX = 1;
-    int mainWinY = 1;
-    WINDOW *mainWin = newwin(mainWinHeight, mainWinWidth, mainWinY, mainWinX);
-    refresh();
+    init_color(COLOR_RED, 255, 255, 0);
+    init_pair(1, COLOR_YELLOW , COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3,COLOR_BLACK,COLOR_WHITE);
+    init_pair(4, COLOR_WHITE,COLOR_BLACK);
 
-    int timerWinWidth = (xMax / 2)-3 ;
-    int timerWinHeight = (yMax/6)-2;
-    int timerWinX = xMax - 79;
-    int timerWinY = 1;
-    WINDOW *timerWin = newwin(timerWinHeight, timerWinWidth, timerWinY, timerWinX);
-    box(timerWin, 0, 0);
-    wrefresh(timerWin);
+    bkgdset(COLOR_PAIR(3));
 
-    int textWinWidth = (xMax / 2)-3 ;
-    int textWinHeight = (yMax/6)-2;
-    int textWinX = 1;
-    int textWinY = 1;
-    WINDOW *textWin = newwin(textWinHeight, textWinWidth, textWinY, textWinX);
-    box(textWin, 0, 0);
-    wrefresh(textWin);
+    int ligne, colonne;
+    getmaxyx(stdscr, ligne, colonne);
 
-    Timer timer = {0, 0, 0};
-    pthread_t timerThread, textThread;
-    pthread_create(&timerThread, NULL, updateTimer, &timer);
-    pthread_create(&textThread, NULL, updateText, textWin);
+    // ASCII art
+    const char *ascii_art[] = {
+        " _____                                                                           _____ ",
+        "( ___ )-------------------------------------------------------------------------( ___ )",
+        " |   |                                                                           |   | ",
+        " |   |                                                                           |   | ",
+        " |   |     ______      _                              ______               _     |   | ",
+        " |   |    (_____ \\    (_)                            (_____ \\             | |    |   | ",
+        " |   |     _____) )___ _  ____ ____     ___  _   _    _____) )___  ____ _ | |    |   | ",
+        " |   |    |  ____/ _  | |/ ___) _  )   / _ \\| | | |  |  ____/ _  )/ ___) || |    |   | ",
+        " |   |    | |   ( ( | | | |  ( (/ /   | |_| | |_| |  | |   ( (/ /| |  ( (_| |    |   | ",
+        " |   |    |_|    \\_||_|_|_|   \\____)   \\___/ \\____|  |_|    \\____)_|   \\____|    |   | ",
+        " |   |                                                                           |   | ",
+        " |___|                                                                           |___| ",
+        "(_____)-------------------------------------------------------------------------(_____)"
+    };
 
-    /* affichage des cartes (dans un cadrillage 4*3)
-    faut archi pas toucher a ça sinon ça part en couille les chefs*/
-
-    int rows = 3;
-    int cols = 4;
-
-    // taille de la carte
-    int cardWidth = 15;
-    int cardHeight = 10;
-    // espacement de la grille
-    int horizontalSpacing = 0;
-    int verticalSpacing = 1;
-
-    int gridWidth = (cols * cardWidth);
-    int gridHeight = (rows * cardHeight) + ((rows - 1) * verticalSpacing);
-
-    int gridX = (xMax - gridWidth) / 2;
-    int gridY = 10;
-
-    // Créer la fenêtre de la grille
-    WINDOW *gridWin = newwin(gridHeight, gridWidth, gridY, gridX);
-    wrefresh(gridWin);
-
-    int cardX = gridX;
-    int cardY = gridY;
-    int selectedX = cardX;
-    int selectedY = cardY;
-    int selectedCard = 0;  // index de la carte sélectionnée
-
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            WINDOW *cardWin = newwin(cardHeight, cardWidth, cardY, cardX);
-            wattron(cardWin, COLOR_PAIR(1));  // Activer la couleur jaune
-            box(cardWin, 0, 0);
-            wattroff(cardWin, COLOR_PAIR(1));  // Désactiver la couleur jaune
-            pthread_mutex_lock(&ncurses_mutex);
-    wrefresh(cardWin);
-    pthread_mutex_unlock(&ncurses_mutex);
-
-            cardX += cardWidth + horizontalSpacing;
-        }
-        cardX = gridX;
-        cardY += cardHeight + verticalSpacing;
-    }
+    const char *menu_items[] = {"Single", "Auto"};
+    int choice;
+    int highlight = 0;
 
     while (1) {
-        int ch = getch();
-        // switch de carte
-        WINDOW *previousCard = newwin(cardHeight, cardWidth, selectedY, selectedX);
-        wattron(previousCard, COLOR_PAIR(1));  // Activer la couleur jaune
-        box(previousCard, 0, 0);
-        wattroff(previousCard, COLOR_PAIR(1));  // Désactiver la couleur jaune
-        wrefresh(previousCard);
+        clear();
 
-        // gestion des inputs
-        if (ch == 'a' && selectedCard > 0) {
-            selectedCard--;
-        } else if (ch == 'e' && selectedCard < (rows * cols - 1)) {
-            selectedCard++;
+        attron(COLOR_PAIR(3));
+        for (int i = 0; i < 13; ++i) {
+            mvprintw(ligne / 8 + i, (colonne - strlen(ascii_art[i])) / 2, ascii_art[i]);
+        }
+        attroff(COLOR_PAIR(highlight + 1));
 
-        }else if(ch == 'q') {
-            break;
+        for (int i = 0; i < 2; ++i) {
+            if (i == highlight) {
+                attron(A_REVERSE);
             }
+            mvprintw(ligne / 2 + i, (colonne - strlen(menu_items[i])) / 2, menu_items[i]);
+            attroff(A_REVERSE);
+        }
+        refresh();
 
-        // calcul de posiitonnement de carte
-        selectedX = gridX + (selectedCard % cols) * (cardWidth + horizontalSpacing);
-        selectedY = gridY + (selectedCard / cols) * (cardHeight + verticalSpacing);
+        choice = getch();
 
-        attron(COLOR_PAIR(1));  // Activer la couleur jaune
-        WINDOW *selectedCardWin = newwin(cardHeight, cardWidth, selectedY, selectedX);
-        box(selectedCardWin, 0, 0);
-        wrefresh(selectedCardWin);
-        attroff(COLOR_PAIR(1));  // Désactiver la couleur jaune
+        switch (choice) {
+            case KEY_UP:
+                highlight = (highlight - 1 + 2) % 2;
+                break;
+            case KEY_DOWN:
+                highlight = (highlight + 1) % 2;
+                break;
+            case 10:
+                if (highlight == 0) {
+                    mvprintw(ligne / 2 + 2, (colonne - strlen("Single option chosen")) / 2, "Single option chosen");
+                    refresh();
+                    sleep(1);
+                    clear();
+                    refresh();
+                    jeu();
+                } else if (highlight == 1) {
+                    mvprintw(ligne / 2 + 2, (colonne - strlen("Auto option chosen")) / 2, "Auto option chosen");
+                    refresh();
+                    sleep(1);
+                }
+                break;
+        }
     }
 
-    delwin(mainWin);
-    delwin(timerWin);
-    delwin(textWin);
-    delwin(gridWin);
-    getch();
     endwin();
-    pthread_cancel(timerThread);
-    pthread_cancel(textThread);
-    pthread_join(timerThread, NULL);
-    pthread_join(textThread, NULL);
-
     return 0;
 }
